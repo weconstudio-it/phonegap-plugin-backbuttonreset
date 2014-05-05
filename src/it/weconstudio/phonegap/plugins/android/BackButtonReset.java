2a5ebeff1f151cb9ccfe474023363d4bb131c2e3
niko <niko@ubuniko.(none)>
2014-05-05 16:39:58 +0200

    uscita su doppio click

--- src/it/weconstudio/phonegap/plugins/android/BackButtonReset.java
+++ src/it/weconstudio/phonegap/plugins/android/BackButtonReset.java
@@ -11,13 +11,16 @@ import android.view.View;
 import android.view.View.OnKeyListener;
 import android.view.KeyEvent;
 import android.view.KeyEvent.Callback;
-
+import java.util.Date;
 
 public class BackButtonReset extends CordovaPlugin {
 
     private static final String LOG_TAG = "BackButtonReset";
 
+    private static Date dateLastBack = null;
 
+    private static final int epsilonDoubleBack = 2000;
+    
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
 
@@ -26,9 +29,20 @@ public class BackButtonReset extends CordovaPlugin {
         webView.setOnKeyListener(new OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
-                    // Do Here whatever you want 
-                	// Log.d(LOG_TAG, "back");
+                	//Log.d(LOG_TAG, "back");
                 	
+                	if(dateLastBack!=null){
+                		//Log.d(LOG_TAG, "data not null");
+                		Date now = new Date();
+                		if(now.getTime() - dateLastBack.getTime() < epsilonDoubleBack){
+                			//Log.d(LOG_TAG, "devo chiudermi");
+                			act.finish();
+                            System.exit(0);
+                		}
+                	}
+                	
+                	dateLastBack = new Date();
+                	//Log.d(LOG_TAG, "data settata");
                 	
                 	act.showWebPage("file:///android_asset/www/index.html", false, true, null);
                 	
